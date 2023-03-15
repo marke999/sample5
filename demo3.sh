30458814adab17e98d076a39795989d19454f30c
Mark <markarnoco31@gmail.com>
2023-03-15 15:22:56 +0800

    Initial commit

---
+++ demo3.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+<html>
+<head>
+
+a=0
+while [ "$a" -lt 10 ]  #this is loop1
+do
+	b="$a"
+	while [ "$b" -ge 0 ] #this is loop2
+	do
+		echo -n "$b"
+		b=`expr $b - 1`
+	done
+	echo
+	a=`expr $a + 1`
+done
+
+</head>
+</html>
